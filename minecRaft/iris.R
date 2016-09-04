ffff0e2b9b19a9ec37a3d973c1b623fd5eb5c8f1
Przemysław Biecek <pbiecek@users.noreply.github.com>
2016-09-04 21:13:19 +0200

    Update iris.R

--- minecRaft/iris.R
+++ minecRaft/iris.R
@@ -1,24 +1,24 @@
 #
-# This will be a usefull function
+# A usefull function
 
 addBlock <- function(x, y, z, b, conn) {
   writeLines(paste0("world.setBlock(",round(x),",",round(y),",",round(z),",",round(b),")"), conn)
 }
 
 #
-# Connect to server
+# Connect to the server (install and run https://www.nostarch.com/download/LTPWM_ch01_update_online.pdf)
 
 conn <- socketConnection(host="localhost", port = 4711, blocking=TRUE, server=FALSE, open="r+")
 baseline <- 70
 
 #
-# Layer of grass and layer of air
+# Add two layers of grass and wipe out everything above
 
 writeLines(paste0("world.setBlocks(-80,",baseline-2,",-80,180,",baseline,",180,2)"), conn)
 writeLines(paste0("world.setBlocks(-50,",baseline+1,",-50,150,",baseline+50,",150,0)"), conn)
 
 #
-# And now add irises
+# And now add blocks based on iris data
 
 for (i in 1:nrow(iris)) {
   addBlock(10*iris[i,"Sepal.Length"],
